258e8e998636fdeca77ca42ef62359ad9aab13ab
daninro <dan.inro@gmail.com>
2014-06-13 16:57:17 -0400

    todos los filtros

--- src/main/java/filter/GenreFilter.java
+++ src/main/java/filter/GenreFilter.java
@@ -2,7 +2,28 @@ package filter;
 
 public class GenreFilter extends Filter{
 
-		public GenreFilter(String genre){
-			super.setFilter("genre = '" + genre + "'");
-		}
+	private String genre2;
+
+			public GenreFilter(String genre, String table){
+					String genero ="SELECT G.* " +
+									"FROM "+table+" AS G " +
+									"WHERE G.id IN (SELECT has.id FROM has WHERE has.name='"+genre+"'))";
+					genre2=genre;
+					super.setFilter(genero);
+			}
+
+			public String getQuery(){
+				String genero ="SELECT G.* " +
+						"FROM has AS G " +
+						"WHERE G.id IN (SELECT has.id FROM has WHERE has.name='"+genre2+"'))";
+
+				return genero;
+			}
+			
+			public String getQuery(String tabla){
+				String genero ="SELECT G.* " +
+						"FROM "+tabla+" AS G " +
+						"WHERE G.id IN (SELECT has.id FROM has WHERE has.name='"+genre2+"'))";
+				return genero;
+			}
 }
